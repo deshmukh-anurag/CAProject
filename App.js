29627c21293ce6e661ad72fd501080f19601ce84
Anurag <anuragdeshmukh61@gmail.com>
2024-11-05 15:17:20 +0530

    finalproject

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { Text, View, StyleSheet, FlatList } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import StackNavigator from "./Navigation/StackNavigator";
 import { AuthProvider } from "./AuthContext";
+import { SocketContextProvider } from "./SocketContext";
 
 const App = () => {
   return (
     <AuthProvider>
+      <SocketContextProvider>
         <StackNavigator />
+      </SocketContextProvider>
     </AuthProvider>
   );
 };
